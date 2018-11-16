dd9399625977014e2622d3f7f054847174113b9d
Rasmus Lystrøm <rasmusl@microsoft.com>
2018-11-16 12:24:56 +0100

    Added claims controller

---
+++ BDSA2018.Lecture11.Web/Controllers/ClaimsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BDSA2018.Lecture11.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClaimsController : ControllerBase
+    {
+        // GET api/claims
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(User.Claims);
+        }
+    }
+}
